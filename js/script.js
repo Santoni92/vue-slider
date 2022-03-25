a218233ba89b5438271575b69011a3ca3f052ebf
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-25 16:22:22 +0100

    finzione con cui aggiungere la classe .active al div contenente l'immagine corrente che si sta visualizzando

--- js/script.js
+++ js/script.js
@@ -51,6 +51,20 @@ const app = new Vue(
                 }else{
                     this.activeSlideIndex = 0;
                 }
+            },
+            checkIfActive(item){    //aggiungo dinamicamente la classe .active al div che contiene la thumbnails
+                //console.log(item.title);
+                const index = this.slides.findIndex(    /*findIndex (è un metodo degli array) ritorna la posizione del primo elemento che soddisfa
+                                                          la condizione(la funzione che gli si passa come parametro e che
+                                                          viene eseguita su ciascun elemento dell'array) e se nessun
+                                                          elemento dell'array soddisfa la condizione allora essa ritorna -1. */
+                    (slide) => {return slide.title === item.title;}
+                )
+                //console.log(index);
+                if(index === this.activeSlideIndex){    //se il valore della const index è uguale al valore dell'indice dalla slide corrente che si sta visualizzando allora aggiungo al div contenente la thumbnails 'attiva'(quella appunto che si sta visualizzando) la classe .active
+                    return 'thumb active';
+                }
+                return 'thumb';
             }
         }
     }
